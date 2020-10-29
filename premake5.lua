a24a526c9544554e2901a2f87466f2cc2b32084d
VMormoris <vmormoris2014@gmail.com>
2020-10-29 14:57:28 +0200

    Creating premake5

---
+++ premake5.lua
@@ -0,0 +1,32 @@
+project "Box2D"
+	kind "StaticLib"
+	language "C++"
+
+	targetdir("%{wks.location}/bin/" .. outputdir .. "/%{prj.name}")
+	objdir("%{wks.location}/bin-int/" .. outputdir .. "/%{prj.name}")
+	
+	files
+	{
+		"src/**.h",
+		"src/**.cpp",
+		"include/**.h"
+	}
+
+	includedirs
+	{
+		"include",
+		"src"
+	}
+	
+	filter "system:windows"
+		systemversion "latest"
+
+	filter "configurations:Debug"
+		runtime "Debug"
+		symbols "on"
+
+	filter "configurations:Release"
+		runtime "Release"
+		optimize "on"
+		
+		defines { "NDEBUG" }
